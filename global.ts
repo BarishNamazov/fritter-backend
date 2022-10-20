96497e7ac662493fb3bee271ba85842cbecbe631
Abutalib (Barish) Namazov <barish2003@gmail.com>
2022-10-20 17:26:44 -0400

    typo

--- global.ts
+++ global.ts
@@ -1,7 +1,7 @@
 import dotenv from 'dotenv';
 dotenv.config({});
 
-const DEBUG: boolean = (process.env.DEV_ENV || 'DEBUG') === 'DEBUG';
+const DEBUG: boolean = (process.env.NODE_ENV || 'DEBUG') === 'DEBUG';
 const PORT = process.env.PORT || 3000;
 const BASEURL = DEBUG ? `http://localhost:${PORT}` : 'https://fritter-demo.vercel.app/';
 const {MONGO_SRV} = process.env;
