67c5694f6a0350e697dc77c6a78a1f306e78bc7e
Abutalib (Barish) Namazov <barish2003@gmail.com>
2022-10-23 20:30:59 -0400

    middlewares for upvote

---
+++ upvote/middleware.ts
@@ -0,0 +1,42 @@
+import type {Request, Response, NextFunction} from 'express';
+
+import UpvoteCollection from './collection';
+
+const getParams = (req: Request) => {
+  if (req.params.freetId) {
+    return [req.params.freetId, 'Freet'];
+  }
+
+  if (req.params.commentId) {
+    return [req.params.commentId, 'Comment'];
+  }
+};
+
+const isUpvoteExists = async (req: Request, res: Response, next: NextFunction) => {
+  const params = getParams(req);
+  const upvote = await UpvoteCollection.findOne(req.session.userId, params[0], params[1] as 'Freet' | 'Comment');
+  if (!upvote) {
+    res.status(404).json({
+      error: 'Vote does not exist.'
+    });
+    return;
+  }
+
+  next();
+};
+
+const isUpvoteValid = async (req: Request, res: Response, next: NextFunction) => {
+  if (!req.body.vote || (req.body.vote !== 'upvote' && req.body.vote !== 'downvote')) {
+    res.status(400).json({
+      error: 'Invalid vote.'
+    });
+    return;
+  }
+
+  next();
+};
+
+export {
+  isUpvoteExists,
+  isUpvoteValid
+};
