b4b3af3366904645b0e332443465727b39705ce0
Abutalib (Barish) Namazov <barish2003@gmail.com>
2022-10-24 00:42:57 -0400

    initial collection for takebreak

---
+++ takebreak/collection.ts
@@ -0,0 +1,23 @@
+import type {HydratedDocument, Types} from 'mongoose';
+
+import type {TakeBreak} from './model';
+import TakeBreakModel from './model';
+
+type MongoId = Types.ObjectId | string;
+
+class TakeBreakCollection {
+  static async startBreak(userId: MongoId): Promise<HydratedDocument<TakeBreak>> {
+    const takeBreak = new TakeBreakModel({userId, dateStart: new Date()});
+    await takeBreak.save();
+    return takeBreak;
+  }
+
+  static async endBreak(userId: MongoId): Promise<HydratedDocument<TakeBreak>> {
+    const takeBreak = await TakeBreakModel.findOne({userId, dateEnd: null});
+    takeBreak.dateEnd = new Date();
+    await takeBreak.save();
+    return takeBreak;
+  }
+}
+
+export default TakeBreakCollection;
