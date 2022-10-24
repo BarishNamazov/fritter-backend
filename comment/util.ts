8fdbf135a0ea59df76f7526d244d403308c91811
Abutalib (Barish) Namazov <barish2003@gmail.com>
2022-10-24 16:31:57 -0400

    comment response constructor

---
+++ comment/util.ts
@@ -0,0 +1,41 @@
+import type {HydratedDocument} from 'mongoose';
+import moment from 'moment';
+import type {Comment, PopulatedComment} from './model';
+
+type CommentResponse = {
+  _id: string;
+  author: string;
+  dateCreated: string;
+  content: string;
+  dateModified: string;
+  freetId: string;
+  numUpvotes: number;
+  numDownvotes: number;
+};
+
+const formatDate = (date: Date): string => moment(date).format('MMMM Do YYYY, h:mm:ss a');
+
+const constructCommentResponse = (comment: HydratedDocument<Comment>): CommentResponse => {
+  const commentCopy: PopulatedComment = {
+    ...comment.toObject({
+      versionKey: false,
+      virtuals: true
+    })
+  };
+  const {username} = commentCopy.authorId;
+  const freetId = commentCopy.freetId._id.toString();
+  delete commentCopy.authorId;
+  delete commentCopy.freetId;
+  return {
+    ...commentCopy,
+    author: username,
+    _id: commentCopy._id.toString(),
+    dateCreated: formatDate(comment.dateCreated),
+    dateModified: formatDate(comment.dateModified),
+    freetId
+  };
+};
+
+export {
+  constructCommentResponse
+};
