b054d9cb66c5e7f7341e33028e034207c0a75dda
Abutalib (Barish) Namazov <barish2003@gmail.com>
2022-10-24 17:00:21 -0400

    middlewares for comment

---
+++ comment/middleware.ts
@@ -0,0 +1,80 @@
+import type {Request, Response, NextFunction} from 'express';
+import {Types} from 'mongoose';
+
+import CommentCollection from './collection';
+
+import {_canUserViewFreet} from '../freet/middleware';
+
+const isValidCommentContent = (req: Request, res: Response, next: NextFunction) => {
+  const {content} = req.body as {content: string};
+  if (!content.trim()) {
+    res.status(400).json({
+      error: 'Comment content must be at least one character long.'
+    });
+    return;
+  }
+
+  next();
+};
+
+const isCommentExists = async (req: Request, res: Response, next: NextFunction) => {
+  if (!req.params.commentId) {
+    res.status(400).json({
+      error: 'Missing comment id.'
+    });
+    return;
+  }
+
+  const comment = await CommentCollection.findOne(req.params.commentId);
+  if (!comment) {
+    res.status(404).json({
+      error: {
+        commentNotFound: `Comment with comment ID ${req.params.commentId} does not exist.`
+      }
+    });
+    return;
+  }
+
+  next();
+};
+
+const isValidCommentModified = async (req: Request, res: Response, next: NextFunction) => {
+  const comment = await CommentCollection.findOne(req.params.commentId);
+  const authorId = comment.authorId._id.toString();
+  if ((req.session.userId as string) !== authorId) {
+    res.status(403).json({
+      error: 'Cannot modify other users\' comments.'
+    });
+    return;
+  }
+
+  next();
+};
+
+const isCommentAccessible = async (req: Request, res: Response, next: NextFunction) => {
+  if (!req.params.commentId) {
+    res.status(400).json({
+      error: 'Missing comment id.'
+    });
+    return;
+  }
+
+  const comment = await CommentCollection.findOne(req.params.commentId);
+  const freet = comment.freetId;
+
+  if (!await _canUserViewFreet(req.session.userId as string, freet._id.toString())) {
+    res.status(403).json({
+      error: 'You are not allowed to view this comment.'
+    });
+    return;
+  }
+
+  next();
+};
+
+export {
+  isValidCommentContent,
+  isCommentExists,
+  isValidCommentModified,
+  isCommentAccessible
+};
